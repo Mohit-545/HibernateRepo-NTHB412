ad401626f7f0ffa238fa59316216dacf9e9b10d7
DELL <DELL@192.168.0.101>
2023-01-16 11:41:55 +0530

    First Project - First Commit

---
+++ HBProj01-saveObject/src/main/java/com/nt/test/SaveObjectTest.java
@@ -0,0 +1,45 @@
+//SaveObjectTest.java (main/client class)
+package com.nt.test;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import com.nt.entity.Product;
+
+public class SaveObjectTest {
+    public static void main( String[] args )  {
+        //BootStrap or Activate the Hibernate
+    		Configuration cfg = new Configuration();
+    	//specify the hibernate cfgs file name and location
+    		cfg.configure("com/nt/cfgs/hibernate.cfg.xml");
+    		Transaction tx = null;  
+    		try (//build SessionFactory obj with all the services  specified in the cfgs file and mapping file
+    	    		SessionFactory factory = cfg.buildSessionFactory();
+    				//create Session object
+    	    		Session ses = factory.openSession();){
+    			//begin Transaction
+    				tx = ses.beginTransaction();  //this internall calls setAutoCommit(false) to disable auto commit on DB s/w
+    				//prepare entity class object
+    					Product prod = new Product();
+    					prod.setPid(1003);	prod.setPname("Mirror");// prod.setPrice(7500.0f); prod.setQty(22.0f);
+    				//save the object
+    					Integer idVal = (Integer) ses.save(prod);
+    					//ses.persist(prod);
+    					System.out.println("The generated IdVal is :: "+idVal);
+    				//commit the transaction
+    					tx.commit();  //this internally calls con.commit() method
+    					System.out.println("Object is saved and Record is Inserted");
+    		}//try
+    		catch (HibernateException hi) {
+    			hi.printStackTrace();
+    			if(tx!=null && tx.getRollbackOnly() && tx.getStatus()!=null) {
+		    			tx.rollback();  //this internally calls con.rollback() method
+		    			System.out.println("Object is not saved and Record is not inserted");
+    			}//if
+			}//catch
+    		
+    }//main
+}//class
